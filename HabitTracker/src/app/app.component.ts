8f785cc29f356e53d689d3b2f9b06853af7266cd
robertdhart99 <robertdhart99@gmail.com>
2024-07-28 15:53:36 -0400

    nav

--- HabitTracker/src/app/app.component.ts
+++ HabitTracker/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,7 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'HabitTracker';
 }
